e860241e1b139f310f4e3ff7d8783dcf15c5aa8a
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2026-02-20 00:25:14 +0100

    feat: send buttons state

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,20 +1,59 @@
-int sliderPin = A0;
+static const uint8_t dataTypeWidth = 1;
 
-int sliderValue = 0;
-int oldSliderValue = 0;
+enum dataType {
+  STATE = 0,
+  SLIDER_VALUE = 1,
+};
 
-void setup() 
-{
+static const uint8_t numberOfButtons = 3;
+static const uint8_t buttonPins[numberOfButtons] = {2, 4, 7};
+
+static const uint8_t sliderPin = A0;
+
+uint8_t state = 0;
+
+uint8_t sliderValue = 0;
+
+void setup() {
   Serial.begin(9600);
+
+  for (uint8_t i = 0; i < numberOfButtons; ++i) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+
+  pinMode(sliderPin, INPUT);
+}
+
+inline uint8_t encode(uint8_t data, enum dataType type) {
+  return data << dataTypeWidth | type;
+}
+
+inline void writeData(uint8_t data, enum dataType type) {
+  Serial.write(encode(data, type));
 }
 
 void loop() {
+  bool stateChanged = false;
+
+  for (uint8_t i = 0; i < numberOfButtons; ++i) {
+    if (digitalRead(buttonPins[i]) == HIGH) {
+      continue;
+    }
+
+    state ^= 1 << i;
+    stateChanged = true;
+  }
+
+  if (stateChanged) {
+    writeData(state, STATE);
+  }
+
+  uint8_t oldSliderValue = sliderValue;
   sliderValue = analogRead(sliderPin) >> 3;
 
   if (oldSliderValue != sliderValue) {
-    Serial.write(sliderValue << 1 | 1);
-    oldSliderValue = sliderValue;
+    writeData(sliderValue, SLIDER_VALUE);
   }
 
-  delay(100);
+  delay(200);
 }
